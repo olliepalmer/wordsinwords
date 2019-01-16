affd878eaf0cc0559b4b0992918a663b8b4a5ebf
Ollie Palmer <github@olliepalmer.com>
2019-01-16 20:35:22 +0100

    Around 2000 new words

--- we put the x in y.py
+++ we put the x in y.py
@@ -6,6 +6,16 @@ def f(s, pat):
 
 # print (f("This is just a simple text to test some basic things", "si"))
 
+def file_len(fname):
+    with open(fname) as f:
+        for i, l in enumerate(f):
+            pass
+    return i + 1
+
+total = file_len('words_alpha.txt')
+print (str(total)+' total words')
+
+counter = 0
 with open('words_alpha.txt','r') as a:
     for wordtofind in a:
         if (len(str.rstrip(wordtofind)) > 1):
@@ -15,3 +25,5 @@ with open('words_alpha.txt','r') as a:
                         text = 'We put the \"'+str.rstrip(wordtofind)+'\" in \"'+str.rstrip(wordtosearch)+'\"\n'
                         with open('we_put_the_x_in_y.txt','a') as out:
                             out.write(text)
+        counter += 1
+        print ('word '+str(counter)+' of '+str(total)) 
